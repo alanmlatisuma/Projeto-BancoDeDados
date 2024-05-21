c1466d7b1a4f033e4bca9c6b257a7904c1771a92
Alan Mantelatto <alanmantelatto@gmail.com>
2024-05-20 22:18:41 -0300

    inserção tabelas - (tcc,professor,discipline)

--- insertion.sql
+++ insertion.sql
@@ -68,3 +68,106 @@ WHERE id IN ('100000007', '100000017', '100000022', '100000026');
 UPDATE student
 SET course_id = '77777'
 WHERE id IN ('100000008', '100000018', '100000023', '100000028', '100000021');
+
+INSERT INTO tcc_group (id)
+VALUES 
+    ('10'),
+    ('20'),
+    ('30');
+
+UPDATE student
+SET tcc_id = '10'
+WHERE id IN ('100000003', '100000016', '100000020');
+
+UPDATE student
+SET tcc_id = '20'
+WHERE id IN ('100000001', '100000002', '100000014', '100000025');
+
+UPDATE student
+SET tcc_id = '30'
+WHERE id IN ('100000008', '100000023', '100000028', '100000021');
+
+INSERT INTO professor (id, name, salary)
+ VALUES
+    ('000001', 'João Silva', 8500.00),
+    ('000002', 'Maria Santos', 9200.00),
+    ('000003', 'Carlos Oliveira', 10150.00),
+    ('000004', 'Ana Pereira', 8700.00),
+    ('000005', 'Pedro Almeida', 11300.00),
+    ('000006', 'Sofia Costa', 10400.00),
+    ('000007', 'Lucas Martins', 12500.00),
+    ('000008', 'Laura Ferreira', 8000.00),
+    ('000009', 'Marcela Rodrigues', 9500.00),
+    ('000010', 'Rafael Cardoso', 12800.00);
+
+UPDATE tcc_group
+SET professor_id = '000001'
+WHERE id IN ('10');
+
+UPDATE tcc_group
+SET professor_id = '000007'
+WHERE id IN ('20');
+
+UPDATE tcc_group
+SET professor_id = '000010'
+WHERE id IN ('30');
+
+INSERT INTO departament (name, boss_id)
+ VALUES
+    ('Matemática', '000001'),
+    ('Sociologia', '000002'),
+    ('Biológicas', '000003'),
+    ('Física', '000004'),
+    ('Finanças', '000006');
+    
+
+UPDATE professor
+SET departament_name = 'Matemática'
+WHERE id IN ('000001');
+
+UPDATE professor
+SET departament_name = 'Sociologia'
+WHERE id IN ('000002');
+
+UPDATE professor
+SET departament_name = 'Biológicas'
+WHERE id IN ('000003');
+
+UPDATE professor
+SET departament_name = 'Física'
+WHERE id IN ('000004');
+
+UPDATE professor
+SET departament_name = 'Finanças'
+WHERE id IN ('000006');
+
+Table discipline {
+  id varchar(6) [primary key]
+  name varchar(25)
+  hours integer(3)
+  class_id varchar(4)
+  departament_name varchar(20)
+}
+
+
+INSERT INTO discipline (id, name, hours, class_id, departament_name)
+ VALUES
+    ('MA0001', 'Álgebra Linear', 60, 'K201', 'Matemática'),
+    ('MA0002', 'Cálculo I', 80, 'K002', 'Matemática'),
+    ('MA0003', 'Geometria Analítica', 40, 'K103', 'Matemática'),
+
+    ('FI0001', 'Mecânica Clássica', 60, 'J001', 'Física'),
+    ('FI0002', 'Eletromagnetismo', 60, 'J102', 'Física'),
+    ('FI0003', 'Termodinâmica', 80, 'J103', 'Física'),
+
+    ('FN0001', 'Gestão Financeira', 40, 'D101', 'Finanças'),
+    ('FN0002', 'Análise de Investimentos', 60, 'K002', 'Finanças'),
+    ('FN0003', 'Contabilidade Gerencial', 60, 'D103', 'Finanças'),
+
+    ('BI0001', 'Biologia Celular', 40, 'J101', 'Biológicas'),
+    ('BI0002', 'Genética', 60, 'K102', 'Biológicas'),
+    ('BI0003', 'Ecologia', 80, 'I103', 'Biológicas'),
+
+    ('SO0001', 'Introdução à Sociologia', 60, 'D101', 'Sociologia'),
+    ('SO0002', 'Teorias Sociológicas', 80, 'K102', 'Sociologia'),
+    ('SO0003', 'Sociologia Urbana', 60, 'K003', 'Sociologia');
